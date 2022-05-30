2e52d9d436a0db2a1e571b674d1047cc00d2fd29
Dutoslav <106549354+Dutoslav@users.noreply.github.com>
2022-05-30 19:21:17 +0200

    Player class + basic Walk function

---
+++ One_Thousand_Miles_Beyond.py
@@ -0,0 +1,15 @@
+terrain_types = {plains:1, hills:1.2}
+
+
+class Player:
+    def __init__(self, name = "Player", distance, fatigue):
+        self.name = name
+        self.distance = 0
+        self.fatigue = 0
+
+    def walk(self, terrain, km):
+        self.distance += km
+        self.fatigue += (km*0.5) * terrain_types[terrain]
+        if fatigue > 100:
+            print("Dead")
+        print("You travelled {km} kms".format(km))
